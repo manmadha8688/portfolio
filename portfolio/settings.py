d73d13fd8c6f5469897d018be7a916e43741490e
Jayamangala Manmadha <manmadhajayamangala777@gmail.com>
2025-03-17 02:02:12 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -111,12 +111,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
